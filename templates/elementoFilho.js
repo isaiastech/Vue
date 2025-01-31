3daf383870219cbacf2fa399cc339e367016bbc4
IsaiasTech <isaiasbatistapy@gmail.com>
2025-01-31 10:34:07 -0300

    Estudando elemento filho

---
+++ templates/elementoFilho.js
@@ -0,0 +1,20 @@
+// Criando uma nova instância do Vue
+const vm = new Vue({
+    // Definindo o elemento no DOM ao qual o Vue será vinculado
+    el: '#app',
+
+    // Definição dos dados do componente (atualmente vazio)
+    data: {},
+
+    // Definição dos métodos do Vue
+    methods: {
+        selecionarElementoFilho() {
+            console.log(inputTexto.type)             // Obtém o tipo do input
+            // console.log(opcao.value)              // Obtém o valor selecionado do select
+            // console.log(paragrafo.style.color)    // Obtém a cor do parágrafo
+            // console.log(recupradiv)              // Obtém o elemento <div> referenciado
+
+
+        }
+    }
+})
